d1938b65393df628ff917375a09c322c80a471dd
zhuyubin <949638039@qq.com>
2018-03-05 09:43:39 +0800

    task: 增加消息队列rabbitmq

---
+++ src/main/java/com/car/ctl/demo/config/RabbitConfig.java
@@ -0,0 +1,14 @@
+package com.car.ctl.demo.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    @Bean
+    public Queue Queue() {
+        return new Queue("carAction");
+    }
+}
---
+++ src/main/java/com/car/ctl/demo/service/MessageSender.java
@@ -0,0 +1,16 @@
+package com.car.ctl.demo.service;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhuyubin
+ * Date: 2018/3/3
+ * Time: 下午5:16
+ * To change this template use File | Settings | File Templates.
+ * Description:
+ */
+public class MessageSender {
+
+    public static void send(Object object){
+        return;
+    }
+}
